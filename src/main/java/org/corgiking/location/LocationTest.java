9e5e60b2ab1b6cce1dd23b06db43e83ae9325333
CorgiKing <1030185489@qq.com>
2018-06-26 18:03:44 +0800

    location

---
+++ src/main/java/org/corgiking/location/LocationTest.java
@@ -0,0 +1,10 @@
+package org.corgiking.location;
+
+public class LocationTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/org/corgiking/resthighclient/IndexTest.java
+++ src/main/java/org/corgiking/resthighclient/IndexTest.java
@@ -31,15 +31,16 @@ public class IndexTest {
 				.builder(new HttpHost("192.168.16.21", 9200, "http"), new HttpHost("192.168.16.22", 9200, "http")));
 
 		indexSourceParams(client);
+		indexContentBuilder(client);
+		indexJsonMap(client);
+		indexJson(client);
 
 		client.close();
 	}
 
 	public static void indexSourceParams(RestHighLevelClient client) throws IOException {
-		IndexRequest indexRequest = new IndexRequest("people", "test", "test1")
-				.source("name", "cyh",
-						"age", 25, "sex",
-						"女", "info", "测试");
+		IndexRequest indexRequest = new IndexRequest("people", "doc", "dba1").source("name", "zhjp", "age", 25, "sex",
+				"男", "info", "DBA");
 		IndexResponse indexResponse = client.index(indexRequest);
 		System.out.println(indexResponse);
 	}
@@ -52,7 +53,7 @@ public class IndexTest {
 		builder.field("sex", "女");
 		builder.field("info", "测试");
 		builder.endObject();
-		IndexRequest indexRequest = new IndexRequest("people", "test", "test1").source(builder);
+		IndexRequest indexRequest = new IndexRequest("people", "doc", "test1").source(builder);
 		IndexResponse indexResponse = client.index(indexRequest);
 		System.out.println(indexResponse);
 	}
@@ -63,14 +64,14 @@ public class IndexTest {
 		jsonMap.put("age", 25);
 		jsonMap.put("sex", "男");
 		jsonMap.put("info", "程序猿");
-		IndexRequest indexRequest = new IndexRequest("people", "dev", "dev2").source(jsonMap);
+		IndexRequest indexRequest = new IndexRequest("people", "doc", "dev1").source(jsonMap);
 		IndexResponse indexResponse = client.index(indexRequest);
 		System.out.println(indexResponse);
 	}
 
 	public static void indexJson(RestHighLevelClient client) throws IOException {
 		People people = new People("yy", 25, "男", "程序员");
-		IndexRequest indexRequest = new IndexRequest("people", "dev", "dev1");
+		IndexRequest indexRequest = new IndexRequest("people", "doc", "dev2");
 		String info = new Gson().toJson(people);
 		indexRequest.source(info, XContentType.JSON);
 		IndexResponse indexResponse = client.index(indexRequest);
